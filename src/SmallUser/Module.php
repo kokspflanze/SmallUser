a45de85f57b95bf27d44f04a1d4c93d99b7e71f5
kokspflanze <kokspflanze@gmx.de>
2014-08-11 20:50:23 +0200

    useless onBoot ....

--- src/SmallUser/Module.php
+++ src/SmallUser/Module.php
@@ -7,11 +7,6 @@ use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
 class Module {
-	public function onBootstrap( MvcEvent $e ) {
-		$eventManager        = $e->getApplication()->getEventManager();
-		$moduleRouteListener = new ModuleRouteListener();
-		$moduleRouteListener->attach( $eventManager );
-	}
 
 	public function getConfig() {
 		return include __DIR__ . '/../../config/module.config.php';
